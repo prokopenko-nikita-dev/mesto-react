3d01d4a4fd90c6f73629d95b6fc158b6022e0fd9
Prokopenko Nikita <117819655+prokopenko-nikita-dev@users.noreply.github.com>
2023-06-01 08:16:34 +0300

    2 version

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -2,7 +2,7 @@ import React from "react"
 
 function ImagePopup({ card, isOpen, onClose }) {
   return (
-    <div className={"popup__image" + (isOpen ? " popup_opened" : "")}>
+    <div className={"popup popup_image" + (isOpen ? " popup_opened" : "")}>
       <div className="popup__container popup__container-img">
         <button type="button" onClick={onClose} className="popup__close popup__close_img"></button>
         <img className="popup__full-img" src={card.link} alt={card.name} />
